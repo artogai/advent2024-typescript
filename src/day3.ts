7ca6c7a28edf72122dd24cdd00d93d77d0967007
Artem Ogai <artem@ogai.dev>
2024-12-04 20:00:20 +0400

    add day3

---
+++ src/day3.ts
@@ -0,0 +1,57 @@
+import { log } from "console";
+import { readLines } from "./utils.js";
+
+const MUL_OP = /mul\((\d+),(\d+)\)/g;
+const MUL_OP_WITH_INSTR = /do\(\)|don't\(\)|mul\((\d+),(\d+)\)/g;
+
+part1();
+part2();
+
+function part1() {
+  const res = readLines("./input/day3.txt").reduce((acc, line) => {
+    return acc + parseMemory(line);
+  }, 0);
+
+  log(res);
+}
+
+function part2() {
+  const res = readLines("./input/day3.txt").reduce((acc, line) => {
+    return acc + parseMemoryWithInstructions(line);
+  }, 0);
+
+  log(res);
+}
+
+function parseMemory(mem: string): number {
+  let acc = 0;
+
+  for (const match of mem.matchAll(MUL_OP)) {
+    acc += Number(match[1]) * Number(match[2]);
+  }
+
+  return acc;
+}
+
+function parseMemoryWithInstructions(mem: string): number {
+  let acc = 0;
+  let enabled = true;
+
+  for (const match of mem.matchAll(MUL_OP_WITH_INSTR)) {
+    switch (match[0]) {
+      case "do()":
+        enabled = true;
+        break;
+      case "don't()":
+        enabled = false;
+        break;
+      default:
+        if (enabled) {
+          acc += Number(match[1]) * Number(match[2]);
+        }
+        break;
+    }
+  }
+
+  return acc;
+}
